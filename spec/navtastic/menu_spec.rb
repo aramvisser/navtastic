1e9f96d9e9d0d2d79580c884ceaed7d3bd5ef432
Aram Visser <hello@aramvisser.com>
2017-08-21 18:13:07 +0700

    Allow submenus to be defined on items
    Any item can now have a submenu when defined.
    
    ```
    Navtastic.define :main do |menu|
      menu.item "Home", '/' do |submenu|
        submenu.item "Posts", '/posts'
      end
    end
    ```

--- spec/navtastic/menu_spec.rb
+++ spec/navtastic/menu_spec.rb
@@ -4,19 +4,39 @@ require 'support/matchers/current_item.rb'
 RSpec.describe Navtastic::Menu do
   subject(:menu) do
     menu = described_class.new
-    menu.item "Home"
-    menu.item "Posts", '/posts'
-    menu.item "Featured", '/posts/featured'
+
+    menu.item "Home" do |submenu|
+      submenu.item "Posts", '/posts'
+      submenu.item "Featured", '/posts/featured'
+    end
+
+    menu.item "About", '/about'
+
     menu
   end
 
   describe '.new' do
     subject(:menu) { described_class.new }
 
-    let(:params) { { x: 1 } }
+    context "when the menu has no parent" do
+      specify { expect(menu.items).to be_empty }
+      specify { expect(menu).not_to have_current_item }
 
-    specify { expect(menu.items).to be_empty }
-    specify { expect(menu).not_to have_current_item }
+      it "has a depth of 0" do
+        expect(menu.depth).to eq 0
+      end
+    end
+
+    context "when the menu has a parent" do
+      subject(:submenu) { described_class.new(menu) }
+
+      specify { expect(submenu.items).to be_empty }
+      specify { expect(submenu).not_to have_current_item }
+
+      it "has a depth of parent.depth + 1" do
+        expect(submenu.depth).to eq(menu.depth + 1)
+      end
+    end
   end
 
   describe '#item' do
@@ -56,15 +76,33 @@ RSpec.describe Navtastic::Menu do
         expect(item.url).to eq url
       end
     end
+
+    context "when the item has a submenu" do
+      let(:item) { menu.items.first }
+
+      it "adds the menu to the itme" do
+        expect(item.submenu).not_to eq nil
+      end
+    end
+  end
+
+  describe '#items' do
+    it "includes every item defined in this menu" do
+      expect(menu.items.count).to eq 2
+    end
   end
 
   describe '#each' do
-    specify { expect { |b| menu.each(&b) }.to yield_control.exactly(3).times }
+    specify { expect { |b| menu.each(&b) }.to yield_control.exactly(2).times }
     specify { expect(menu).to all be_a Navtastic::Item }
   end
 
   describe '#[]' do
-    it "returns the item for the given url" do
+    it "returns the item for the given url in this menu" do
+      expect(menu['/about'].name).to eql 'About'
+    end
+
+    it "returns the item for the given url in a submenu" do
       expect(menu['/posts/featured'].name).to eql 'Featured'
     end
 
@@ -91,10 +129,34 @@ RSpec.describe Navtastic::Menu do
     context "when the current url was set" do
       before { menu.current_url = url }
 
-      let(:url) { '/posts' }
+      let(:url) { '/about' }
 
       it "points to the item matching the url" do
         expect(current_item).to eq menu[url]
+      end
+    end
+
+    context "when the current url matches an item in a submenu" do
+      before { menu.current_url = url }
+
+      let(:url) { '/posts' }
+
+      it "points to the item in the submenu" do
+        expect(current_item).to eq menu[url]
+      end
+    end
+
+    context "when the current menu has a parent" do
+      before { menu.current_url = url }
+
+      subject(:submenu) { menu.items.first.submenu }
+
+      let(:url) { '/about' }
+
+      it "asks the parent menu for the current item" do
+        allow(menu).to receive :current_item
+        submenu.current_item
+        expect(menu).to have_received :current_item
       end
     end
   end
@@ -128,8 +190,6 @@ RSpec.describe Navtastic::Menu do
       subject(:menu) do
         menu = described_class.new
         menu.item "Home", '/'
-        menu.item "Posts", '/posts'
-        menu.item "Featured", '/posts/featured'
         menu
       end
 
