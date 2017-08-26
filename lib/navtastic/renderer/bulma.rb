325740f25bf075280f33bd7e2dcfa41ca02e895a
Aram Visser <hello@aramvisser.com>
2017-08-26 07:46:20 +0700

    Add bulma.io link to Bulma demo page

--- lib/navtastic/renderer/bulma.rb
+++ lib/navtastic/renderer/bulma.rb
@@ -6,11 +6,11 @@ module Navtastic
     # @see file:README.md#Bulma documentation on renderer options
     class Bulma < Navtastic::Renderer
       def submenu_inside_container?(item)
-        !(labels? && item.menu.root?)
+        !(headers? && item.menu.root?)
       end
 
       def menu_tag(menu)
-        if labels? && menu.root?
+        if headers? && menu.root?
           nav(class: 'menu') { yield }
         else
           ul(class: 'menu-list') { yield }
@@ -18,7 +18,7 @@ module Navtastic
       end
 
       def item_tag(item)
-        if labels? && item.menu.root?
+        if headers? && item.menu.root?
           para(class: 'menu-label') { yield }
         else
           li { yield }
@@ -35,8 +35,8 @@ module Navtastic
 
       private
 
-      def labels?
-        options[:root_labels]
+      def headers?
+        options[:headers]
       end
     end
   end
